2857d96076b8f6a21a5d3c5a48ec329a000c323b
dutianze <dutianzer@gmail.com>
2024-07-14 23:23:46 +0900

    markdown save

--- src/main/frontend/components/MilkdownEditor.tsx
+++ src/main/frontend/components/MilkdownEditor.tsx
@@ -1,14 +1,18 @@
-import { CmdKey } from '@milkdown/core';
-import { redoCommand, undoCommand } from '@milkdown/plugin-history';
+import { CmdKey, Editor, defaultValueCtx, rootCtx } from '@milkdown/core';
+import { clipboard } from '@milkdown/plugin-clipboard';
+import { history, redoCommand, undoCommand } from '@milkdown/plugin-history';
+import { listener, listenerCtx } from '@milkdown/plugin-listener';
+import { upload } from '@milkdown/plugin-upload';
 import {
+  commonmark,
   toggleEmphasisCommand,
   toggleStrongCommand,
   wrapInBlockquoteCommand,
   wrapInBulletListCommand,
   wrapInOrderedListCommand
 } from '@milkdown/preset-commonmark';
-import { toggleStrikethroughCommand } from '@milkdown/preset-gfm';
-import { Milkdown } from '@milkdown/react';
+import { gfm, toggleStrikethroughCommand } from '@milkdown/preset-gfm';
+import { Milkdown, useEditor } from '@milkdown/react';
 import { callCommand } from '@milkdown/utils';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
@@ -16,16 +20,41 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import RedoIcon from '@mui/icons-material/Redo';
+import SaveIcon from '@mui/icons-material/Save';
 import StrikethroughSIcon from '@mui/icons-material/StrikethroughS';
 import UndoIcon from '@mui/icons-material/Undo';
-import { Box, Container, IconButton, Toolbar } from '@mui/material';
-import { useMilkdownEditor } from 'Frontend/hooks/useEditor';
+import { Box, IconButton, Stack } from '@mui/material';
 import { useState } from 'react';
 
 export default function MilkdownEditor() {
   const [content, setContent] = useState('');
 
-  const { get } = useMilkdownEditor('');
+  const useMilkdownEditor = () =>
+    useEditor((root) =>
+      Editor.make()
+        .config((ctx) => {
+          ctx.set(rootCtx, root);
+          ctx.set(defaultValueCtx, content);
+          ctx
+            .get(listenerCtx)
+            .mounted((ctx) => {
+              const wrapper = ctx.get(rootCtx) as HTMLDivElement;
+              const editor = wrapper.querySelector(
+                ".editor[role='textbox']"
+              ) as HTMLDivElement;
+              wrapper.onclick = () => editor?.focus();
+            })
+            .markdownUpdated((_, markdown) => setContent(markdown));
+        })
+        .use(commonmark)
+        .use(listener)
+        .use(clipboard)
+        .use(history)
+        .use(upload)
+        .use(gfm)
+    );
+
+  const { get } = useMilkdownEditor();
 
   function call<T>(command: CmdKey<T>, payload?: T) {
     return get()?.action(callCommand(command, payload));
@@ -40,14 +69,18 @@ export default function MilkdownEditor() {
         flexDirection: 'column'
       }}
     >
-      <Container
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={1}
         sx={{
-          width: '100%',
           borderBottom: 1,
-          borderColor: 'divider'
+          borderColor: 'divider',
+          width: '100%'
         }}
       >
-        <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Stack direction="row" spacing={1}>
           <IconButton onClick={() => call(undoCommand.key)}>
             <UndoIcon />
           </IconButton>
@@ -72,17 +105,23 @@ export default function MilkdownEditor() {
           <IconButton onClick={() => call(wrapInBlockquoteCommand.key)}>
             <FormatQuoteIcon />
           </IconButton>
-        </Toolbar>
-      </Container>
-      <Container
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <IconButton onClick={(e) => console.log(content)}>
+            <SaveIcon />
+          </IconButton>
+        </Stack>
+      </Stack>
+      <Box
         sx={{
           height: '100%',
+          width: '100%',
           overflow: 'auto',
           overscrollBehavior: 'none'
         }}
       >
         <Milkdown />
-      </Container>
+      </Box>
     </Box>
   );
 }
